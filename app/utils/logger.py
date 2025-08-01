65fc7a4c665c458d2fdeaae1d52db34f0fcc12ce
zrdt80 <maciejpawlicki10@gmail.com>
2025-08-01 23:25:32 +0200

    fix: close session after adding the log

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -5,7 +5,7 @@ from app.database import SessionLocal
 
 
 def log_event(event: str):
-    db: Session = SessionLocal()
-    log = models.Log(timestamp=datetime.utcnow(), event=event)
-    db.add(log)
-    db.commit()
+    with SessionLocal() as db:
+        log = models.Log(timestamp=datetime.utcnow(), event=event)
+        db.add(log)
+        db.commit()
